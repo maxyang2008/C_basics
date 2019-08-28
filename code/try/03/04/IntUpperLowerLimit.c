e8c5ef0b7cad87929024c96207703b698fa36222
maxyang2008 <maxyang2008@163.com>
2019-08-28 21:14:06 +0800

    Create IntUpperLowerLimit.c

---
+++ code/try/03/04/IntUpperLowerLimit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+int main(void)
+{
+	unsigned int iNumber1;
+	iNumber1 = 1314;
+	printf("%d\n", iNumber1);
+	
+
+	int Imax;
+	int Imin;
+	int C;
+
+	Imax = INT_MAX;
+	Imin = INT_MIN;
+	C = 2147483647;
+	printf("Int max is: %d\n", Imax);
+	printf("Int min is: %d\n", Imin);
+	printf("Int min is: %d\n", C);
+}
+
