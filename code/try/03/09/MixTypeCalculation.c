8d7be4dfcbc7a81231bbb12674fe2c526f765a02
maxyang2008 <maxyang2008@163.com>
2019-08-29 20:43:05 +0800

    Create MixTypeCalculation.c

---
+++ code/try/03/09/MixTypeCalculation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	//int result;
+	//float result;
+	double result;
+	result = (10 + 'a') - 1.5 + 3.2 * 6;
+	printf("%f", result);
+	return EXIT_SUCCESS;
+}
+
