9d8727e6e7ba12f92d89e88fbe81d4e9864135c9
maxyang2008 <maxyang2008@163.com>
2019-08-29 20:11:24 +0800

    Create 74520.c

---
+++ code/try/03/07/74520.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	char a;
+	char b;
+	char c;
+	char d;
+	char e;
+
+	a = 55;
+	b = 52;
+	c = 53;
+	d = 50;
+	e = 48;
+
+	printf("%c%c%c%c%c", a, b, c, d, e);
+	return EXIT_SUCCESS;
+}
